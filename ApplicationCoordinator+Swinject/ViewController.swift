5c060ccabddd1b4fb9c3d526f51bc4aa66d1288d
Sho <sho.weblife@gmail.com>
2019-11-05 01:34:56 +0900

    Initial Commit

---
+++ ApplicationCoordinator+Swinject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplicationCoordinator+Swinject
+//
+//  Created by Shota Ito on 2019/11/05.
+//  Copyright © 2019 Shota Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
